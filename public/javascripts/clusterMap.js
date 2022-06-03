fe2c2ee4280dc4d24a009659826e37d4a451c849
Sanyam-02 <sanyamagrawal7431@gmail.com>
2022-06-03 23:11:51 +0530

    .

--- public/javascripts/clusterMap.js
+++ public/javascripts/clusterMap.js
@@ -2,8 +2,8 @@ mapboxgl.accessToken = mapToken;
 const map = new mapboxgl.Map({
     container: 'cluster-map',
     style: 'mapbox://styles/mapbox/light-v10',
-    center: [-103.5917, 40.6699],
-    zoom: 3
+    center: [79.0882, 21.1458],
+    zoom: 4
 });
 
 map.addControl(new mapboxgl.NavigationControl(),'bottom-right');
